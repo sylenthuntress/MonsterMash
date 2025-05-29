74f67c645b221738b6004423346a6158e22a764b
SylentHuntress <GaymerGirlEva@gmail.com>
2025-05-29 16:38:47 -0500

    Decentralize variant behavior from specific mobs
    Signed-off-by: SylentHuntress <GaymerGirlEva@gmail.com>

--- src/main/java/sylenthuntress/monstermash/content/variant/behavior/ReaperVariantBehavior.java
+++ src/main/java/sylenthuntress/monstermash/content/variant/behavior/ReaperVariantBehavior.java
@@ -1,4 +1,4 @@
-package sylenthuntress.monstermash.content.variant.skeleton;
+package sylenthuntress.monstermash.content.variant.behavior;
 
 import sylenthuntress.monstermash.content.variant.VariantBehavior;
 
--- src/main/java/sylenthuntress/monstermash/registry/MobVariants.java
+++ src/main/java/sylenthuntress/monstermash/registry/MobVariants.java
@@ -3,22 +3,17 @@ package sylenthuntress.monstermash.registry;
 import net.minecraft.registry.Registry;
 import sylenthuntress.monstermash.MonsterMash;
 import sylenthuntress.monstermash.content.variant.VariantBehavior;
-import sylenthuntress.monstermash.content.variant.skeleton.ReaperVariantBehavior;
+import sylenthuntress.monstermash.content.variant.behavior.ReaperVariantBehavior;
 
 public class MobVariants {
+    public static final VariantBehavior REAPER = register("reaper", new ReaperVariantBehavior());
+
+
     public static VariantBehavior register(String id, VariantBehavior variant) {
-        return Registry.register(ModRegistries.VARIANT_BEHAVIORS, MonsterMash.modIdentifier("variant/" + id), variant);
+        return Registry.register(ModRegistries.VARIANT_BEHAVIORS, MonsterMash.modIdentifier("variant_behavior/" + id), variant);
     }
 
     public static void registerAll() {
 
-    }
-
-    public static class SkeletonVariants {
-        public static final VariantBehavior REAPER = register("reaper", new ReaperVariantBehavior());
-
-        public static VariantBehavior register(String id, VariantBehavior variant) {
-            return MobVariants.register("skeleton/" + id, variant);
-        }
     }
 }
