8ac89313765edb9a293e827108cfa5ebaaa3e6e4
SylentHuntress <GaymerGirlEva@gmail.com>
2025-05-29 16:01:42 -0500

    Initial commit

---
+++ src/main/java/sylenthuntress/monstermash/MonsterMash.java
@@ -0,0 +1,20 @@
+package sylenthuntress.monstermash;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MonsterMash implements ModInitializer {
+    public static final String MOD_ID = "monstermash";
+    public static final String MOD_NAME = "MonsterMash";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    public static Identifier modIdentifier(String id) {
+        return Identifier.of(MOD_ID, id);
+    }
+
+    public void onInitialize() {
+        MonsterMash.LOGGER.info(MOD_NAME + " successfully loaded!");
+    }
+}
